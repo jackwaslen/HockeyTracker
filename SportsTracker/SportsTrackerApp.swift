aa20588d338c4a11a0b08b23ced60c48b911d958
jackwaslen <wasl8390@mylaurier.ca>
2024-01-15 14:31:18 -0500

    Initial Commit

---
+++ SportsTracker/SportsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsTrackerApp.swift
+//  SportsTracker
+//
+//  Created by John Waslen on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
